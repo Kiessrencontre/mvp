256d21b81559078d1aaca57745668bc06b3f69d6
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2024-06-17 17:31:34 +0200

    add setup kiess

--- api/models/card.js
+++ api/models/card.js
@@ -1,12 +1,6 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
 const cardSchema = new Schema({
-    owner_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     revealed: { type: Boolean, default: false },
     points_required: { type: Number, default: 0 }
 });
-
 const Card = mongoose.model('Card', cardSchema);
-
-module.exports = Card;
